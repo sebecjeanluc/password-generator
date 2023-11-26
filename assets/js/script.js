2aca3cf4d22270f60c359078118885c1e13b1821
sebecjeanluc <tkawamura11@gmail.com>
2023-11-26 02:37:33 +0000

    refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Array of special characters to be included in password
-var specialCharacters = [
+const specialCharacters = [
 	'@',
 	'%',
 	'+',
@@ -26,10 +26,10 @@ var specialCharacters = [
 ]
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 // Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
+const lowerCasedCharacters = [
 	'a',
 	'b',
 	'c',
@@ -59,7 +59,7 @@ var lowerCasedCharacters = [
 ]
 
 // Array of uppercase characters to be included in password
-var upperCasedCharacters = [
+const upperCasedCharacters = [
 	'A',
 	'B',
 	'C',
@@ -88,62 +88,60 @@ var upperCasedCharacters = [
 	'Z',
 ]
 
-var outOfRangeMessage =
+// Error message for user input
+const outOfRangeMessage =
 	'Your number is not within the range. Click the button again.'
 
-var noneOfCharacterTypesMessage =
-	'You may accept one of the character types. Please hit the button again to start over.'
+const noneOfCharacterTypesMessage =
+	'You may accept one of the character types. Please start over.'
 
-// store the character type questions in object
-var questionsToUser = {
-	SpecialCharacter: function (answer) {
-		var userCharacterChoice = confirm(
+// Function to confirm character types for the password
+const characterTypeConfirmation = {
+	SpecialCharacter: function () {
+		let userCharacterChoice = confirm(
 			'Would you like your password include Special characters?'
 		)
 		return userCharacterChoice
 	},
-	Numeric: function (answer) {
-		var userNumericChoice = confirm(
+	Numeric: function () {
+		let userNumericChoice = confirm(
 			'Would you like your password include numbers?'
 		)
 		return userNumericChoice
 	},
-	Uppercase: function (answer) {
-		var userNumericChoice = confirm(
+	Uppercase: function () {
+		let userNumericChoice = confirm(
 			'Would you like your password include Uppercase?'
 		)
 		return userNumericChoice
 	},
-	Lowercase: function (answer) {
-		var userNumericChoice = confirm(
+	Lowercase: function () {
+		let userNumericChoice = confirm(
 			'Would you like your password include Lowercase?'
 		)
 		return userNumericChoice
 	},
 }
 
-// console.log(getMultipleArrayItems(128))
-
-// Function to prompt user for password options
+// Function to get user input for password options
 function getPasswordOptions() {
-	// Initiatize userInputLength
-	var userInputPasswordLength = 0
-	var atLeastOneTypeSelected = false
-	var isValidLength = false
-	var userCharacterChoices = {}
+	let userInputLength = 0
+	let atLeastOneTypeSelected = false
+	let isValidLength = false
+	let userCharacterChoices = {}
 
 	while (!isValidLength || !atLeastOneTypeSelected) {
-		var lengthInput = prompt(
+		let lengthInput = prompt(
 			"Please type your password's length between 8 - 128. You will get error if your choice is out range."
 		)
 		// trim the space and convert a string to int
-		userInputPasswordLength = parseInt(lengthInput.trim())
+		userInputLength = parseInt(lengthInput.trim())
 
 		// handling non number or string answer
 		if (
-			!isNaN(userInputPasswordLength) &&
-			userInputPasswordLength >= 8 &&
-			userInputPasswordLength <= 128
+			!isNaN(userInputLength) &&
+			userInputLength >= 8 &&
+			userInputLength <= 128
 		) {
 			isValidLength = true
 		} else {
@@ -153,18 +151,19 @@ function getPasswordOptions() {
 		}
 
 		// comfirms user of four character types
-		for (var question in questionsToUser) {
-			userCharacterChoices[question] = questionsToUser[question]()
+		for (let question in characterTypeConfirmation) {
+			userCharacterChoices[question] = characterTypeConfirmation[question]()
 		}
 
 		// Validate if user select at least one character type
 		atLeastOneTypeSelected = Object.values(userCharacterChoices).includes(true)
 
 		if (!atLeastOneTypeSelected) {
-			return alert(noneOfCharacterTypesMessage)
+			alert(noneOfCharacterTypesMessage)
+			continue
 		}
 		var passwordOptions = {
-			length: userInputPasswordLength,
+			length: userInputLength,
 			choices: userCharacterChoices,
 		}
 	}
@@ -174,39 +173,49 @@ function getPasswordOptions() {
 // Function for getting a random element from an array
 function getRandomCharacterFromArray(array) {
 	// Generate a random index based on the length of the array
-	var randomIndex = Math.floor(Math.random() * array.length)
+	let randomIndex = Math.floor(Math.random() * array.length)
 	return array[randomIndex]
 }
 
-// pick the true choices of random number from array, fill the rest of the length of number of user length
+// Shuffle items better in array
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[array[i], array[j]] = [array[j], array[i]]
+	}
+}
+
+// Function to generate a random password based on selected options
 function generatePassword() {
-	var password = ''
-	var passwordOption = getPasswordOptions()
-	var selectedArray = []
-	var randomArrayItems = []
+	let options = getPasswordOptions()
+	let randomArrayItems = []
+	let selectedCharacters = []
+	let password = ''
 
-	if (passwordOption.choices.SpecialCharacter) {
+	if (options.choices.SpecialCharacter) {
 		//if true
-		selectedArray = selectedArray.concat(specialCharacters)
+		selectedCharacters = selectedCharacters.concat(specialCharacters)
 	}
-	if (passwordOption.choices.Numeric) {
-		selectedArray = selectedArray.concat(numericCharacters)
-	}
-
-	if (passwordOption.choices.Uppercase) {
-		selectedArray = selectedArray.concat(upperCasedCharacters)
+	if (options.choices.Numeric) {
+		selectedCharacters = selectedCharacters.concat(numericCharacters)
 	}
 
-	if (passwordOption.choices.Lowercase) {
+	if (options.choices.Uppercase) {
+		selectedCharacters = selectedCharacters.concat(upperCasedCharacters)
+	}
+
+	if (options.choices.Lowercase) {
 		//if true
-		selectedArray = selectedArray.concat(lowerCasedCharacters)
+		selectedCharacters = selectedCharacters.concat(lowerCasedCharacters)
 	}
 	// Shuffle the array of the combined array inside
-	selectedArray.sort(() => Math.random() - 0.5)
+	// SelectedCharacters.sort(() => Math.random() - 0.5)
+	// Using Fisher-Yates algorithm methods since the one above wasnt strong
+	shuffleArray(selectedCharacters)
 
 	// generate the random characters for the rest.
-	for (var i = 0; passwordOption.length > i; i++) {
-		randomArrayItems.push(selectedArray[i % selectedArray.length])
+	for (let i = 0; options.length > i; i++) {
+		randomArrayItems.push(selectedCharacters[i % selectedCharacters.length])
 	}
 	// combine user's selection and the rest array
 	password = randomArrayItems.join('')
@@ -215,16 +224,14 @@ function generatePassword() {
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate')
+const generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword()
-	var passwordText = document.querySelector('#password')
+	let password = generatePassword()
+	let passwordText = document.querySelector('#password')
 
 	passwordText.value = password
-
-	return
 }
 
 // Add event listener to generate button
